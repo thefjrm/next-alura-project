45d7321cfbf3939f3c0540bd9e00f1a5ecd64e0c
thefjrm <felipemartinsplayernet@gmail.com>
2022-05-03 20:05:06 -0300

    Estilização das páginas

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+/*
+import React from "react";
 import Link from "../src/components/Link";
+import Footer from "../src/components/Footer/index";
+
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+    </React.Fragment>
+  );
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Title as="h1">Home Page</Title>
       <Link href="/faq">Ir para o FAQ</Link>
+      <Footer />
     </div>
   );
 }
+*/
